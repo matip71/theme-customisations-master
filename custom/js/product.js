29b1769c9ec263b7ad344a359ca4b441818ca6aa
matip71 <matipereyra92@hotmail.com>
2026-04-24 20:03:58 -0300

    Product sticky bar fix for price and button

--- custom/js/product.js
+++ custom/js/product.js
@@ -30,6 +30,55 @@
 	}
 
 	/**
+	 * Sync the sticky add-to-cart bar with the selected variation.
+	 * - Updates the price shown in the sticky bar.
+	 * - Changes the button from "Seleccionar opciones" to "Agregar al carrito"
+	 *   when a valid variation is found, and makes it submit the form.
+	 * - Reverts everything on variation reset.
+	 */
+	function bindStickyBar() {
+		var $form = $('.product form.variations_form');
+		if (!$form.length) return;
+
+		var $stickyPrice = $('.storefront-sticky-add-to-cart__content-price');
+		var $stickyBtn = $('.storefront-sticky-add-to-cart__content-button');
+		if (!$stickyPrice.length || !$stickyBtn.length) return;
+
+		var originalPriceHTML = $stickyPrice.html();
+		var originalBtnText = $stickyBtn.text().trim();
+		var originalHref = $stickyBtn.attr('href');
+
+		$form.on('show_variation', function (e, variation) {
+			// 1. Update price
+			$stickyPrice.html(variation.price_html);
+
+			// 2. If variation is in stock, convert to add-to-cart button
+			if (variation.is_in_stock) {
+				$stickyBtn
+					.text('Agregar al carrito')
+					.attr('href', '#')
+					.off('click.stickyATC')
+					.on('click.stickyATC', function (ev) {
+						ev.preventDefault();
+						// Submit the main product form
+						$form.find('.single_add_to_cart_button').trigger('click');
+					});
+			}
+		});
+
+		$form.on('hide_variation reset_data', function () {
+			// Revert price
+			$stickyPrice.html(originalPriceHTML);
+
+			// Revert button
+			$stickyBtn
+				.text(originalBtnText)
+				.attr('href', originalHref)
+				.off('click.stickyATC');
+		});
+	}
+
+	/**
 	 * Show/hide custom sizing fields based on the selected size.
 	 * - Shows fields when "A medida" is selected.
 	 * - Hides fields when any other size is selected.
@@ -59,6 +108,7 @@
 
 	$(function () {
 		bindVariationPrice();
+		bindStickyBar();
 		bindCustomSizing();
 	});
 
